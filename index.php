951b1e86ea326cff3c01ac701274b7f12de5e12e
iaspersonalshauss11-beep <iaspersonalshauss11@gmail.com>
2026-01-07 16:27:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,207 +1,87 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 <title>Zozor: la página web</title>
 </head>
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-<link rel="stylesheet" href="css/los.css">
-<link rel="stylesheet" href="css/cam.css">
-<link rel="stylesheet" href="css/carr1.css"/>
-<link rel="stylesheet" href="css/luna.css"/>
+<style type="text/css">
+  *{
+    margin: 0px;
+    padding: 0px;
+
+  }
+  .general{
+    margin: auto;
+    margin-top: 50px;
+    width: 80%;
+    max-width: 1000px;
+    height: 800px;
+  }
+  .oferta{
+    width: 30%;
+    padding: 20px;
+    box-sizing: border-box;
+    background: #ccc;
+    float: left;
+  }
+  .novedades{
+    width: 70%;
+    background:#fff;
+    padding: 20px;
+    float: left;
+    box-sizing: border-box;
+  }
+  .ho{
+    width: 50%;
+    height: auto;
+    
+  }
+  .titu{
+    background: #EB4A33;
+    margin: 20px 0;
+    text-align:center;
+  }
+  .body{
+    clear: both;
+    float: left;
+    margin-top: 20px;
+    box-sizing: border-box;
+    width: 100%;
+    padding: 20px;
+    color: #fff;
+    background: #000;
+  }
+  @media screen and (max-width:800px){
+   .novedades{
+    width: 100%;
+  }
+  .oferta{
+    width: 100%;
+  }
+  
+   }
+  @media screen and (max-width: 400px){
+    .general{
+      width: 100%;
+    }
+  }
+</style>
 <body>
 <div class="general">
-  <div class="sub1-sub1"><img src="img/carita.jpg" alt="" /></div>
-  <div class="sub1-sub2">
-    <div class=""><h2>Aunque no sea conmigo</h2></div>
-    <div class=""><h2>Pero quiero verte Feliz</h2></div>
-  </div>
-  <div class="sub1-sub3"><img src="img/carita.jpg" alt="" /></div>
-
-  <div class="subgeneral1">
-    <div class="carrusel">
-      <div class="carrusel-items">
-        <div class="carrusel-item">
-          <img src="img/un16.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un17.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un18.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un19.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un20.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un021.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un22.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un23.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un24.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un25.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un26.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un27.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un28.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un29.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un30.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un31.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un37.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un33.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un34.jpg" alt="" />
-        </div>
-        <div class="carrusel-item">
-          <img src="img/un35.jpg" alt="" />
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="sub01">
-    <div class="cloud1"> 
-        <img src="img/bebi4.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub02">
-    <div class="cloud2"> 
-        <img src="img/bebi2.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub03">
-    <div class="cloud3"> 
-        <img src="img/bebi3.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub04">
-    <div class="cloud4"> 
-        <img src="img/bebi1.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub05">
-    <div class="cloud5"> 
-        <img src="img/bebi4.png" alt="" />
-      </div>
-  </div>
-  <div class="sub06">
-    <div class="cloud6"> 
-        <img src="img/bebi2.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub07">
-    <div class="cloud7"> 
-        <img src="img/bebi3.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub08">
-    <div class="cloud8"> 
-        <img src="img/bebi1.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub09">
-    <div class="cloud9"> 
-        <img src="img/bebi4.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub010">
-    <div class="cloud10"> 
-        <img src="img/bebi2.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub011">
-    <div class="cloud11"> 
-        <img src="img/bebi3.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub012">
-    <div class="cloud12"> 
-        <img src="img/bebi1.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub013">
-    <div class="cloud13"> 
-        <img src="img/bebi4.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub014">
-    <div class="cloud14"> 
-        <img src="img/bebi2.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub015">
-    <div class="cloud15"> 
-        <img src="img/bebi3.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub016">
-    <div class="cloud16"> 
-        <img src="img/bebi1.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub017">
-    <div class="cloud17"> 
-        <img src="img/bebi4.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub018">
-    <div class="cloud18"> 
-        <img src="img/bebi2.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub019">
-    <div class="cloud19"> 
-        <img src="img/bebi1.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub020">
-    <div class="cloud20"> 
-        <img src="img/bebi4.jpg" alt="" />
-      </div>
-  </div>
-  <div class="sub021">
-    <div class="cloud21"> 
-        <img src="img/bebi2.jpg" alt="" />
-      </div>
-  </div>
-  <div class="subgeneral2"></div>
-  <div class="subgeneral3">
-    <div class="basevideo">
-      <video class="video"  controls>
-         <source src="video/edicion.mp4" type="video/mp4">
-         <source src="video/video.webm" type="video/webm">
-       </video>
-
-    </div>
-
-  </div>
-  <div class="subgeneral4"></div>
-
-  <script src="js/jv.js"></script>
-  
+  <h1 class="titu">titular de mi sitio web</h1>
+  <div class="oferta">a historia del Perú es la historia del territorio del actual Perú, que abarca desde los restos más antiguos de ocupación humana, hasta nuestros días. Los primeros grupos humanos llegaron hacia fines de la glaciación wisconsiense (XI milenio a. C.) como cazadores-recolectores (periodo lítico). Sus descendientes empezaron a desarrollar la horticultura hacia el VIII milenio a. C. (periodo arcaico), época en la que también se domesticaron los camélidos sudamericanos y empezaron a aparecer las primeras aldeas.</div>
+  <div class="novedades">
+  <img class="ho" src="frase.jpg">
+  <br>
+  El nacimiento de la civilización en el Perú corresponde a la civilización Caral, cuyo centro fue la ciudad de Caral, en la costa central peruana. Esta cultura se desarrolló entre 3200 y 1800 a. C.. Es la más antigua de las civilizaciones de América y contemporánea de otros grandes centros de irradiación cultural del mundo, como Sumeria, Egipto, China e India.</div>
+  <div class="body">Desvanecida la civilización Caral hacia el 1800 a. C., esta dio paso a nuevas centros culturales en la costa, al norte y al sur. Surgió la cultura de Cupisnique y posteriormente, hacia el 1200 a. C., la Chavín, un importante centro que articuló las sociedades agrícolas de su época hasta el 200 a. C. El templo de Chavín de Huántar fue sin duda un importante santuario del Antiguo Perú que atraía a numerosos peregrinos. Tal debió ser la importancia de Chavín, de carácter religioso y cultural, más que política o militar.</div>
+</div>
+<video width="640" height="360" controls autoplay preload>
+ 
+<source src="triste.mp4" type='video/mp4; codecs="avc1,mp4a"' />
+<source src="mivideo.ogv" type='video/ogg; codecs="theora,vorbis"' />
+<source src="mivideo.webm" type='video/webm; codecs="vp8,vorbis"' />
+ 
+</video>
 </body>
 </html>
