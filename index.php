5381c0130a7c2e04217bcfd5aa596b2e91cec444
iaspersonalshauss11-beep <iaspersonalshauss11@gmail.com>
2026-01-06 19:23:19 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,207 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title>Zozor: la página web</title>
+</head>
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<link rel="stylesheet" href="css/los.css">
+<link rel="stylesheet" href="css/cam.css">
+<link rel="stylesheet" href="css/carr1.css"/>
+<link rel="stylesheet" href="css/luna.css"/>
+<body>
+<div class="general">
+  <div class="sub1-sub1"><img src="img/carita.jpg" alt="" /></div>
+  <div class="sub1-sub2">
+    <div class=""><h2>Aunque no sea conmigo</h2></div>
+    <div class=""><h2>Pero quiero verte Feliz</h2></div>
+  </div>
+  <div class="sub1-sub3"><img src="img/carita.jpg" alt="" /></div>
+
+  <div class="subgeneral1">
+    <div class="carrusel">
+      <div class="carrusel-items">
+        <div class="carrusel-item">
+          <img src="img/un16.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un17.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un18.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un19.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un20.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un021.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un22.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un23.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un24.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un25.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un26.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un27.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un28.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un29.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un30.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un31.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un37.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un33.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un34.jpg" alt="" />
+        </div>
+        <div class="carrusel-item">
+          <img src="img/un35.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="sub01">
+    <div class="cloud1"> 
+        <img src="img/bebi4.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub02">
+    <div class="cloud2"> 
+        <img src="img/bebi2.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub03">
+    <div class="cloud3"> 
+        <img src="img/bebi3.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub04">
+    <div class="cloud4"> 
+        <img src="img/bebi1.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub05">
+    <div class="cloud5"> 
+        <img src="img/bebi4.png" alt="" />
+      </div>
+  </div>
+  <div class="sub06">
+    <div class="cloud6"> 
+        <img src="img/bebi2.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub07">
+    <div class="cloud7"> 
+        <img src="img/bebi3.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub08">
+    <div class="cloud8"> 
+        <img src="img/bebi1.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub09">
+    <div class="cloud9"> 
+        <img src="img/bebi4.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub010">
+    <div class="cloud10"> 
+        <img src="img/bebi2.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub011">
+    <div class="cloud11"> 
+        <img src="img/bebi3.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub012">
+    <div class="cloud12"> 
+        <img src="img/bebi1.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub013">
+    <div class="cloud13"> 
+        <img src="img/bebi4.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub014">
+    <div class="cloud14"> 
+        <img src="img/bebi2.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub015">
+    <div class="cloud15"> 
+        <img src="img/bebi3.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub016">
+    <div class="cloud16"> 
+        <img src="img/bebi1.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub017">
+    <div class="cloud17"> 
+        <img src="img/bebi4.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub018">
+    <div class="cloud18"> 
+        <img src="img/bebi2.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub019">
+    <div class="cloud19"> 
+        <img src="img/bebi1.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub020">
+    <div class="cloud20"> 
+        <img src="img/bebi4.jpg" alt="" />
+      </div>
+  </div>
+  <div class="sub021">
+    <div class="cloud21"> 
+        <img src="img/bebi2.jpg" alt="" />
+      </div>
+  </div>
+  <div class="subgeneral2"></div>
+  <div class="subgeneral3">
+    <div class="basevideo">
+      <video class="video"  controls>
+         <source src="video/edicion.mp4" type="video/mp4">
+         <source src="video/video.webm" type="video/webm">
+       </video>
+
+    </div>
+
+  </div>
+  <div class="subgeneral4"></div>
+
+  <script src="js/jv.js"></script>
+  
+</body>
+</html>
